08279daa9354a4fa831f73435c9a4b7105640f97
Suralc <thesurwaveing@googlemail.com>
2014-12-27 03:28:23 +0100

    Reduce code nesting depth in 5.4 NodeWalker.

--- src/PhpParser/AnalysingNodeWalkers/Php54LanguageFeatureNodeWalker.php
+++ src/PhpParser/AnalysingNodeWalkers/Php54LanguageFeatureNodeWalker.php
@@ -32,7 +32,7 @@ use Pvra\RequirementAnalysis\Result\RequirementReason;
  * * Array function dereferencing
  * * Callable typhint: `callable`
  * * Detection of `$this` in closures
- * 
+ *
  * @package Pvra\PhpParser\AnalysingNodeWalkers
  */
 class Php54LanguageFeatureNodeWalker extends LanguageFeatureAnalyser implements RequirementAnalyserAwareInterface
@@ -49,85 +49,17 @@ class Php54LanguageFeatureNodeWalker extends LanguageFeatureAnalyser implements 
      */
     public function enterNode(Node $node)
     {
-        if ($node instanceof Node\Stmt\Trait_) {
-            $this->getResult()->addRequirement(
-                RequirementReason::TRAIT_DEFINITION,
-                $node->getLine()
-            );
-        } elseif ($node instanceof Node\Stmt\TraitUse) {
-            $this->getResult()->addRequirement(
-                RequirementReason::TRAIT_USE,
-                $node->getLine()
-            );
-        } elseif ($node instanceof Node\Scalar\MagicConst\Trait_) {
-            $this->getResult()->addRequirement(
-                RequirementReason::TRAIT_MAGIC_CONST,
-                $node->getLine()
-            );
-        } elseif ($node instanceof Node\Expr\ArrayDimFetch) {
-            if ($node->var instanceof Node\Expr\FuncCall
-                || $node->var instanceof Node\Expr\MethodCall
-                || $node->var instanceof Node\Expr\StaticCall
-            ) {
-                $this->getResult()->addRequirement(
-                    RequirementReason::ARRAY_FUNCTION_DEREFERENCING,
-                    $node->getLine()
-                );
-            } elseif ($this->inClosureLevel > 0
-                && $node->var instanceof Node\Expr\Variable && $node->var->name === 'this'
-            ) {
-                $this->_addThisInClosure($node->var);
-            }
-        } elseif ($node instanceof Node\Stmt\Function_
-            || $node instanceof Node\Stmt\ClassMethod
-            || $node instanceof Node\Expr\Closure
-        ) {
-            if (!empty($node->params)) {
-                foreach ($node->params as $param) {
-                    if ((string)$param->type === 'callable') {
-                        $this->getResult()->addRequirement(
-                            RequirementReason::TYPEHINT_CALLABLE,
-                            $param->getLine()
-                        );
-                    }
-                }
-            }
-        } elseif ($node instanceof Node\Expr\MethodCall) {
-            if ($node->var instanceof Node\Expr\New_) {
-                $this->getResult()->addRequirement(
-                    RequirementReason::INSTANT_CLASS_MEMBER_ACCESS,
-                    $node->getLine()
-                );
-            } elseif ($this->inClosureLevel > 0
-                && $node->var instanceof Node\Expr\Variable && $node->var->name === 'this'
-            ) {
-                $this->_addThisInClosure($node->var);
-            }
-        } elseif ($this->inClosureLevel > 0 && $node instanceof Node\Expr\PropertyFetch
-            && $node->var instanceof Node\Expr\Variable && $node->var->name === 'this'
-        ) {
-            $this->_addThisInClosure($node->var);
-        } elseif ($this->inClosureLevel > 0 && $node instanceof Node\Expr\FuncCall
-            && $node->name instanceof Node\Expr\Variable && $node->name->name === 'this'
-        ) {
-            $this->_addThisInClosure($node->name);
-        } elseif ($node instanceof Node\Scalar\LNumber) {
-            if ($node->hasAttribute('originalValue')) {
-                if (stripos($node->getAttribute('originalValue'), '0b') !== false) {
-                    $this->getResult()->addRequirement(RequirementReason::BINARY_NUMBER_DECLARATION, $node->getLine());
-                }
-            } else {
-                throw new \LogicException("Missing attribute");
-            }
-        } elseif ($node instanceof Node\Expr\Array_) {
-            if (!$node->hasAttribute('traditionalArray')) {
-                $this->getResult()->addRequirement(RequirementReason::SHORT_ARRAY_DECLARATION, $node->getLine());
-            }
-        }
-
         if ($node instanceof Node\Expr\Closure) {
             $this->inClosureLevel++;
         }
+
+        $this->handleTraitFeatures($node);
+        $this->handleFunctionDereferencing($node);
+        $this->handleCallableType($node);
+        $this->handleInstantClassMemberAccess($node);
+        $this->handleThisInClosure($node);
+        $this->handleBinaryNumberDeclaration($node);
+        $this->handleShortArrayDeclaration($node);
     }
 
     /**
@@ -151,8 +83,128 @@ class Php54LanguageFeatureNodeWalker extends LanguageFeatureAnalyser implements 
      *
      * @param \PhpParser\Node $node
      */
-    private function _addThisInClosure(Node $node)
+    private function addThisInClosureRequirement(Node $node)
     {
         $this->getResult()->addRequirement(RequirementReason::THIS_IN_CLOSURE, $node->getLine());
+    }
+
+    /**
+     * @param \PhpParser\Node $node
+     */
+    private function handleTraitFeatures(Node $node)
+    {
+        if ($node instanceof Node\Stmt\Trait_) {
+            $this->getResult()->addRequirement(
+                RequirementReason::TRAIT_DEFINITION,
+                $node->getLine()
+            );
+        } elseif ($node instanceof Node\Stmt\TraitUse) {
+            $this->getResult()->addRequirement(
+                RequirementReason::TRAIT_USE,
+                $node->getLine()
+            );
+        } elseif ($node instanceof Node\Scalar\MagicConst\Trait_) {
+            $this->getResult()->addRequirement(
+                RequirementReason::TRAIT_MAGIC_CONST,
+                $node->getLine()
+            );
+        }
+    }
+
+    /**
+     * @param \PhpParser\Node $node
+     */
+    private function handleFunctionDereferencing(Node $node)
+    {
+        if ($node instanceof Node\Expr\ArrayDimFetch && ($node->var instanceof Node\Expr\FuncCall
+                || $node->var instanceof Node\Expr\MethodCall
+                || $node->var instanceof Node\Expr\StaticCall)
+        ) {
+            $this->getResult()->addRequirement(
+                RequirementReason::ARRAY_FUNCTION_DEREFERENCING,
+                $node->getLine()
+            );
+        }
+    }
+
+    /**
+     * @param \PhpParser\Node $node
+     */
+    private function handleCallableType(Node $node)
+    {
+        if (($node instanceof Node\Stmt\Function_
+                || $node instanceof Node\Stmt\ClassMethod
+                || $node instanceof Node\Expr\Closure)
+            && !empty($node->params)
+        ) {
+            foreach ($node->params as $param) {
+                if ((string)$param->type === 'callable') {
+                    $this->getResult()->addRequirement(
+                        RequirementReason::TYPEHINT_CALLABLE,
+                        $param->getLine()
+                    );
+                }
+            }
+
+        }
+    }
+
+    /**
+     * @param \PhpParser\Node $node
+     */
+    private function handleInstantClassMemberAccess(Node $node)
+    {
+        if ($node instanceof Node\Expr\MethodCall && $node->var instanceof Node\Expr\New_) {
+            $this->getResult()->addRequirement(
+                RequirementReason::INSTANT_CLASS_MEMBER_ACCESS,
+                $node->getLine()
+            );
+        }
+    }
+
+    /**
+     * @param \PhpParser\Node $node
+     */
+    private function handleThisInClosure(Node $node)
+    {
+        if ($this->inClosureLevel > 0) {
+            if (($node instanceof Node\Expr\PropertyFetch || $node instanceof Node\Expr\ArrayDimFetch || $node instanceof Node\Expr\MethodCall)
+                && $node->var instanceof Node\Expr\Variable
+                && $node->var->name === 'this'
+            ) {
+                $this->addThisInClosureRequirement($node->var);
+            } elseif ($node instanceof Node\Expr\FuncCall
+                && $node->name instanceof Node\Expr\Variable
+                && $node->name->name === 'this'
+            ) {
+                $this->addThisInClosureRequirement($node->name);
+            }
+        }
+    }
+
+    /**
+     * @param \PhpParser\Node $node
+     */
+    private function handleBinaryNumberDeclaration(Node $node)
+    {
+        if ($node instanceof Node\Scalar\LNumber) {
+            if ($node->hasAttribute('originalValue')) {
+                if (stripos($node->getAttribute('originalValue'), '0b') !== false) {
+                    $this->getResult()->addRequirement(RequirementReason::BINARY_NUMBER_DECLARATION, $node->getLine());
+                }
+            } else {
+                throw new \LogicException("Missing attribute");
+            }
+        }
+    }
+
+    /**
+     * @param \PhpParser\Node $node
+     */
+    private function handleShortArrayDeclaration(Node $node)
+    {
+        if ($node instanceof Node\Expr\Array_ && $node->getAttribute('traditionalArray', false) !== true) {
+            $this->getResult()->addRequirement(RequirementReason::SHORT_ARRAY_DECLARATION, $node->getLine());
+        }
     }
 }
